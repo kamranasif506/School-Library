ecfd5d455006214ebe8768183d62167849c8c973
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-08-28 17:49:34 +0500

    resolved linter errors

--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,12 @@
 require './person'
 
 class Teacher < Person
-    def initialize(age, parent_permission = true, name = "Unknown", specialization)
-        super(age, parent_permission, name)
-        @specialization = specialization
-    end
+  def initialize(age, parent_permission = true, name = 'Unknown', specialization)
+    super(age, parent_permission, name)
+    @specialization = specialization
+  end
 
-    def can_use_services?
-        true
-    end
+  def can_use_services?
+    true
+  end
 end
