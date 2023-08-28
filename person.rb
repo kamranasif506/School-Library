a5c8cfa987efc0fd2bfdca40ee791add85015337
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-08-28 17:45:08 +0500

    update person class for getter and setter methods

--- person.rb
+++ person.rb
@@ -8,14 +8,6 @@ class Person
         @age = age
         @parent_permission = parent_permission
     end
-
-    def name=(new_name)
-        @name = new_name
-    end
-    
-    def age=(new_age)
-        @age = new_age
-    end
     
     def can_use_services?
         of_age? || @parent_permission
