5b146f05234594b7b0d24ab20dd8ac1e79a68a78
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-09-05 12:44:10 +0500

    SOLID principals applied

--- app.rb
+++ app.rb
@@ -99,44 +99,4 @@ class App
     end
     puts "\n"
   end
-
-  def render_choices
-    puts 'Please Choose an Option by entering a number:'
-    puts '1. List all books.'
-    puts '2. List all people.'
-    puts '3. Create a person.'
-    puts '4. Create a book.'
-    puts '5. Create a rental.'
-    puts '6. List all rentals for a given person id.'
-    puts '7. EXIT'
-  end
-
-  def choose_number(choice)
-    case choice
-    when 1
-      list_all_books
-    when 2
-      list_all_people
-    when 3
-      create_a_person
-    when 4
-      add_new_book
-    when 5
-      add_rental
-    when 6
-      list_all_rentals
-    end
-  end
-
-  def show
-    loop do
-      render_choices
-      choice = gets.chomp.to_i
-      if choice >= 7
-        puts 'Thank you for using this app'
-        break
-      end
-      choose_number(choice)
-    end
-  end
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,45 @@
 require_relative 'app'
 
+def render_choices
+  puts 'Please Choose an Option by entering a number:'
+  puts '1. List all books.'
+  puts '2. List all people.'
+  puts '3. Create a person.'
+  puts '4. Create a book.'
+  puts '5. Create a rental.'
+  puts '6. List all rentals for a given person id.'
+  puts '7. EXIT'
+end
+
+def choose_number(choice, app)
+  case choice
+  when 1
+    app.list_all_books
+  when 2
+    app.list_all_people
+  when 3
+    app.create_a_person
+  when 4
+    app.add_new_book
+  when 5
+    app.add_rental
+  when 6
+    app.list_all_rentals
+  end
+end
+
+
 def main
-  my_app = App.new
-  my_app.show
+  app = App.new
+  loop do
+    render_choices
+    choice = gets.chomp.to_i
+    if choice >= 7
+      puts 'Thank you for using this app'
+      break
+    end
+    choose_number(choice,app)
+  end
 end
 
 main
