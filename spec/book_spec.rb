a5c56cb7ab08cace9ad80dafa0870ca2d81d3757
Ichsan Sandypratama <ichsan2668@gmail.com>
2023-09-06 21:58:32 +0700

    Add classroom, teacher ,decorator unit test

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../book'
 
-describe Book do
+RSpec.describe Book do
     it "book" do
         book = Book.new('Avengers', 'Tony Stark')
         expect(book.title).to eql('Avengers')  
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -1,8 +1,25 @@
 require_relative '../person'
 
-describe Person do
-    it "person" do
-        person = Person.new(12, parent_permission: true, name: 'Unknown')
-        expect(person.age).to eql(12)  
+RSpec.describe Person do
+    before :each do
+        @person = Person.new(12, parent_permission: true, name: 'Ichsan')
+    end
+    
+    describe "#age" do
+        it "should be 12" do
+            expect(@person.age).to eql(12)  
+        end
+    end
+
+    describe "#parent_permission" do
+        it "should be true" do
+            expect(@person.parent_permission).to eql(true)  
+        end
+    end
+
+    describe "#name" do
+        it "should be ichsan" do
+            expect(@person.name).to eql("Ichsan")  
+        end
     end
 end
